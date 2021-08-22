de36baba909a7ba2e29ff554e90411309b8b48a2
Daniele Di Modica <d.dimo94@hotmail.com>
2021-08-22 19:10:06 +0100

    Implemented simple AI aiming system.

--- Source/SimpleShooter/ShooterAIController.cpp
+++ Source/SimpleShooter/ShooterAIController.cpp
@@ -2,4 +2,16 @@
 
 
 #include "ShooterAIController.h"
+#include "Kismet/GameplayStatics.h"
 
+
+
+void AShooterAIController::BeginPlay() 
+{
+    Super::BeginPlay();
+
+    //Get hold of the pawn Actor
+    APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
+
+    SetFocus(PlayerPawn);
+}
